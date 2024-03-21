18637d97d620eb9b9b2eded2755e02f4f5a95eff
ihnat13 <ignatfomich7@gmail.com>
2024-03-21 16:35:40 +0200

    swap to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,93 @@
-// import React, {Component} from 'react';
-// import './App.css';
+import React, {Component} from 'react';
+import './App.css';
 
-// const  = () => {
-//     return (
-//       <div>
-//         helloworld
-//       </div>
-//     );
-// }
+const App = () => {
+    return (
+        <div>
+        <nav  class="navbar navbar-expand-lg navbar-light bg-light sticky-top bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img class="img-fluid img-thumbnail" width="100" height="100" src="../../FULLSTACK PROJ/img/logo.png" alt=""></img>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Головна</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Меню</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Про нас</a>
+              </li>
+            </ul>
+          </div>
+            <a class="nav-link" href="#">Корзина</a>
+        </div>
+      </nav>
+    
+      <div class="container my-5">
+        <h1 class="text-center mb-4">Ласкаво просимо до Піца Кафе!</h1>
+        <p class="lead text-center mb-4">Ми пропонуємо найкращі піци та каву у місті. Замовте вже зараз!</p>
+      </div>
+      <div class="container my-5">
+        <h1 class="text-center mb-4">Ласкаво просимо до Піца Кафе!</h1>
+        <p class="lead text-center mb-4">Ми пропонуємо найкращі піци та каву у місті. Замовте вже зараз!</p>
+      </div> <div class="container my-5">
+        {/* <!-- Содержимое главной страницы --> */}
+        <h1 class="text-center mb-4">Ласкаво просимо до Піца Кафе!</h1>
+        <p class="lead text-center mb-4">Ми пропонуємо найкращі піци та каву у місті. Замовте вже зараз!</p>
+        {/* <!-- Добавьте фотографии и текст акций --> */}
+      </div> <div class="container my-5">
+        {/* <!-- Содержимое главной страницы --> */}
+        <h1 class="text-center mb-4">Ласкаво просимо до Піца Кафе!</h1>
+        <p class="lead text-center mb-4">Ми пропонуємо найкращі піци та каву у місті. Замовте вже зараз!</p>
+        {/* <!-- Добавьте фотографии и текст акций --> */}
+      </div> <div class="container my-5">
+        {/* <!-- Содержимое главной страницы --> */}
+        <h1 class="text-center mb-4">Ласкаво просимо до Піца Кафе!</h1>
+        <p class="lead text-center mb-4">Ми пропонуємо найкращі піци та каву у місті. Замовте вже зараз!</p>
+        {/* <!-- Добавьте фотографии и текст акций --> */}
+      </div> <div class="container my-5">
+        {/* <!-- Содержимое главной страницы --> */}
+        <h1 class="text-center mb-4">Ласкаво просимо до Піца Кафе!</h1>
+        <p class="lead text-center mb-4">Ми пропонуємо найкращі піци та каву у місті. Замовте вже зараз!</p>
+        {/* <!-- Добавьте фотографии и текст акций --> */}
+      </div> <div class="container my-5">
+        {/* <!-- Содержимое главной страницы --> */}
+        <h1 class="text-center mb-4">Ласкаво просимо до Піца Кафе!</h1>
+        <p class="lead text-center mb-4">Ми пропонуємо найкращі піци та каву у місті. Замовте вже зараз!</p>
+        {/* <!-- Добавьте фотографии и текст акций --> */}
+      </div> <div class="container my-5">
+        {/* <!-- Содержимое главной страницы --> */}
+        <h1 class="text-center mb-4">Ласкаво просимо до Піца Кафе!</h1>
+        <p class="lead text-center mb-4">Ми пропонуємо найкращі піци та каву у місті. Замовте вже зараз!</p>
+        {/* <!-- Добавьте фотографии и текст акций --> */}
+      </div> <div class="container my-5">
+        {/* <!-- Содержимое главной страницы --> */}
+        <h1 class="text-center mb-4">Ласкаво просимо до Піца Кафе!</h1>
+        <p class="lead text-center mb-4">Ми пропонуємо найкращі піци та каву у місті. Замовте вже зараз!</p>
+        {/* <!-- Добавьте фотографии и текст акций --> */}
+      </div> <div class="container my-5">
+        {/* <!-- Содержимое главной страницы --> */}
+        <h1 class="text-center mb-4">Ласкаво просимо до Піца Кафе!</h1>
+        <p class="lead text-center mb-4">Ми пропонуємо найкращі піци та каву у місті. Замовте вже зараз!</p>
+        {/* <!-- Добавьте фотографии и текст акций --> */}
+      </div>
+    
+      {/* <!-- Добавьте блоки для других страниц (Меню, Про нас, Корзина) --> */}
+    
+      <footer class="bg-dark text-light py-4">
+        <div class="container text-center">
+          <p>© 2024 Піца Кафе. Усі права захищені.</p>
+        </div>
+      </footer>
+      </div>
+    );
+}
 
 
-// export default 
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App.jsx';
-// const root = document.getElementById('root');
-// const rootContainer = ReactDOM.createRoot(root)
-// rootContainer.render( 
-//     <App /> 
-// );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+const root = document.getElementById('root');
+const rootContainer = ReactDOM.createRoot(root)
+rootContainer.render( 
+    <App /> 
+);
